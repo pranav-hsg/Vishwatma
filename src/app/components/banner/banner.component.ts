535bd549f9796eeb5b6f84eac875508aad431777
Pranav <pranavprati@gmail.com>
2021-05-16 17:15:10 +0530

    Search Bar Added

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -11,5 +11,27 @@ export class BannerComponent implements OnInit {
   
   ngOnInit(): void {
   }
+  debounce(func: Function, wait : number) {
+    let timeout: any;
+    return function(...aargs: any) {
+     /*    console.log(args,args2 */
+      let later = function() {
+        timeout = null;
+        func.apply(this,aargs);
+      };
+      var callNow = !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(this, aargs);
+    };
+  };
+  search(e){
+      console.log("Indeed",e)
+      console.log('skipped')
+  }
+  onKeyPress(e:any){
+     let search = this.debounce(this.search,2000); 
+    //  search(e)
+  }
   
 }
